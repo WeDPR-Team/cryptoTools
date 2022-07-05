2fe2e188e0ba39ed7680ddd6f4d79d712c5a0d4f
shareong <740310627@qq.com>
2022-07-05 20:49:24 +0800

    add curve defination

---
+++ cryptoTools/Crypto/DefaultCurve.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <cryptoTools/Common/Defines.h>
+
+#if defined(ENABLE_SODIUM) || defined(ENABLE_RELIC)
+
+#if defined(ENABLE_SODIUM)
+#include "SodiumCurve.h>
+#elif defined(ENABLE_RELIC)
+#include "RCurve.h"
+#endif
+
+namespace osuCrypto
+{
+    // Declare aliases for the default elliptic curve implementation.
+    namespace DefaultCurve
+    {
+#if defined(ENABLE_SODIUM)
+        using Point = Sodium::Rist25519;
+        using Number = Sodium::Prime25519;
+        // Allow declaring a Curve variable. Constructor is to suppress unused variable warnings.
+        struct Curve { Curve() {} };
+#elif defined(ENABLE_RELIC)
+        using Curve = REllipticCurve;
+        using Point = REccPoint;
+        using Number = REccNumber;
+#endif
+    }
+}
+
+#endif
